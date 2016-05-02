2adb5f911eacc26d790c06fadbd6d22351351b63
AJ W <ayjay122@gmail.com>
2016-05-01 22:01:51 -0500

    Fixes issue with example where child views don't have key attributes.

--- example/index.js
+++ example/index.js
@@ -4,11 +4,11 @@
     Song = require('../src/Song').default;
 
 
-  var songPropsForEdit = { name: 'Billie Jean', edit: true, placeholder: 'Song name', onChange: function (e) { console.log('A change happened: %s', e.nativeEvent.target.value); }},
+  var songPropsForEdit = { key: 2, name: 'Billie Jean', edit: true, placeholder: 'Song name', onChange: function (e) { console.log('A change happened: %s', e.nativeEvent.target.value); }},
 
     container = document.createElement('div'),
 
-    songElementReadOnly = React.createElement(Song, { name: 'Hotel California', edit: false }),
+    songElementReadOnly = React.createElement(Song, { key: 1, name: 'Hotel California', edit: false }),
     songElementEdit = React.createElement(Song, songPropsForEdit),
 
     simpleRoot = React.createElement('div', null, [songElementReadOnly, songElementEdit]);
